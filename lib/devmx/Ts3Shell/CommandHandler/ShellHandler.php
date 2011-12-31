d0b69f38d86386646ccacfda8a3c52c46b495a18
drak3 <drak3@live.de>
2011-12-31 01:24:55 +0100

    fixed cd

--- lib/devmx/Ts3Shell/CommandHandler/ShellHandler.php
+++ lib/devmx/Ts3Shell/CommandHandler/ShellHandler.php
@@ -1,9 +1,4 @@
 <?php
-
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 namespace devmx\Ts3Shell\CommandHandler;
 use Symfony\Component\Process\Process;
 use Symfony\Component\Process\ProcessBuilder;
@@ -38,6 +33,19 @@ class ShellHandler implements CommandHandlerInterface
         $out = trim($proc->getErrorOutput());
         if($out !== '') {
            $resp->getErrorOutput()->writeln('<error>'.$out.'</error>'); 
+        }
+        if($cmd->getName() == 'cd' && $proc->isSuccessful()){
+            $args = $cmd->getArguments();
+            if(!isset($args[1])) {
+                chdir(getenv('HOME'));
+            }
+            else {
+                $dir = trim($args[1]);
+                if(isset($dir[0]) && $dir[0] == '~') {
+                    $dir = preg_replace('/~/',getenv('HOME'),$dir,1);
+                }
+                chdir($dir);
+            }
         } 
         return $resp;
     }
